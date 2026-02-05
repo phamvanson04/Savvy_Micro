c7f296acd9eef7d771d75c3defd20e55c482725b
CRELLA <domanhcuong27062003@gmail.com>
2026-02-05 16:14:30 +0700

    crud student

--- auth-service/src/main/java/com/savvycom/auth_service/AuthServiceApplication.java
+++ auth-service/src/main/java/com/savvycom/auth_service/AuthServiceApplication.java
@@ -3,11 +3,14 @@ package com.savvycom.auth_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication(scanBasePackages = {"com.savvycom", "com.savvy.common"})
 public class AuthServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AuthServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Ho_Chi_Minh"));
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
 
 }
