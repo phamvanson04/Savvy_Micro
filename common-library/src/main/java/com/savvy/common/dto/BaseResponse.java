e28f30e52fbf58457f4e13adb51aacbb934fcc13
Owen <vanson2004tkhd@gmail.com>
2026-02-09 08:06:08 +0000

    chore: merge main into greade-service and update BaseResponse

--- common-library/src/main/java/com/savvy/common/dto/BaseResponse.java
+++ common-library/src/main/java/com/savvy/common/dto/BaseResponse.java
@@ -1,14 +1,18 @@
 package com.savvy.common.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class BaseResponse<T> {
     private boolean success;
